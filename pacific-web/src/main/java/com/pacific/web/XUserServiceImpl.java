c44d54306c3a3b4472af4f749968347391cc2396
litiebiao <litiebiao@shining3d.com>
2016-06-18 14:30:27 +0800

    commit!

---
+++ pacific-web/src/main/java/com/pacific/web/XUserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.pacific.web;
+
+import com.pacific.common.web.xuser.XUser;
+import com.pacific.common.web.xuser.XUserService;
+
+/**
+ * Created by Fe on 16/6/18.
+ */
+public class XUserServiceImpl implements XUserService {
+    @Override
+    public String getPass(String name) {
+        return null;
+    }
+
+    @Override
+    public Long getUid(String name) {
+        return null;
+    }
+
+    @Override
+    public void loginSuccess(XUser xUser) {
+
+    }
+
+    @Override
+    public String configName() {
+        return null;
+    }
+}
